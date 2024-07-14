a15c4de00f9f7a77abddefe4f9cef0e89aa008c4
germontenegro27 <german2717@gmail.com>
2024-07-14 16:19:25 -0300

    first commit 2

--- src/app/page.js
+++ src/app/page.js
@@ -20,18 +20,6 @@ export default function Home() {
   }, []);
 
   return (
-  //   <div className="container mx-auto px-4 py-8">
-  //     <h1 className="text-4xl font-bold mb-4 text-gray-800">Blog</h1>
-  //     {loading ? (
-  //       <div className="text-center text-gray-600">Loading...</div>
-  //     ) : (
-  //       posts.length > 0 ? (
-  //         <PostList posts={posts} />
-  //       ) : (
-  //         <div className="text-center text-gray-600">No posts found</div>
-  //       )
-  //     )}
-  //   </div>
   <div className="bg-gray-100 min-h-screen">
   <div className="container mx-auto px-4 py-8">
     <h1 className="text-4xl font-bold mb-8 text-gray-800">Explora nuestro Blog</h1>
